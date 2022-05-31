900d59acbb0255f20d69293f975d0f640f065cf8
RyanGar46 <RyanGar46@gmail.com>
2022-05-31 17:52:35 -0400

    Add settings
    Ports over my library code from some of my python projects

---
+++ jsetting/settings.py
@@ -0,0 +1,86 @@
+import json
+import os
+from typing import Any
+
+
+class Settings:
+    def __init__(self, version: int):
+        self._properties = {}
+        self.add_property("meta", "version", version)
+
+    def add_property(self, group: str, key: str, default=None) -> "Settings":
+        """
+        Add a property to be stored in the settings
+
+        :param group: A section of properties
+        :param key: The name of the property
+        :param default: The default value of the property
+        :return: Its self
+        """
+        if group in self._properties:
+            self._properties[group] |= {key: default}
+        else:
+            self._properties |= {group: {key: default}}
+        return self
+
+    def set_property(self, group: str, key: str, value: Any) -> "Settings":
+        """
+        Set the value of a property
+
+        :param group: A section of properties
+        :param key: The name of the property
+        :param value: The value that the property will be set to
+        :return: Its self
+        """
+        self._properties[group][key] = value
+        return self
+
+    def get_property(self, group: str, key: str):
+        """
+        Get the value of a property
+
+        :param group: A section of properties
+        :param key: The name of the property
+        :return: The value stored in the property
+        """
+        if group in self._properties:
+            if key in self._properties[group]:
+                return self._properties[group][key]
+            else:
+                raise AttributeError(f"No property called '{key}' in '{group}'")
+        else:
+            raise AttributeError(f"No properties in {group}")
+
+    def is_property_unset(self, group: str, key: str) -> bool:
+        """
+        Whether the selected property is unset
+
+        :param group: A section of properties
+        :param key: The name of the property
+        :return: True is unset. False if set
+        """
+        return self.get_property(group, key) is None
+
+    def get_group(self, group: str) -> dict:
+        """
+        Get the value of a property
+
+        :param group: A section of properties
+        :return: The value stored in the property
+        """
+        if group in self._properties:
+            return self._properties[group]
+        else:
+            raise AttributeError(f"No properties in {group}")
+
+    def save(self):
+        with open("settings.json", "w", encoding="utf-8") as file:
+            json.dump(self._properties, file, ensure_ascii=False, indent=2)
+
+    def load(self):
+        if os.path.exists("settings.json"):
+            with open("settings.json", "r") as file:
+                settings_file = json.load(file)
+            for group, properties in settings_file.items():
+                for key, value in properties.items():
+                    self.add_property(group, key, value)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="jsetting",
+    packages=find_packages(),
+    version="1.0.0",
+    description="A JSON settings library for Python 3",
+    author="Ryan Garrett",
+    license="GNU GPLv3"
+)
